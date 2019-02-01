3331509a763d23ed0db252bd0f933e27bfb4f464
nyameo <nyameo>
2019-02-01 17:37:58 +0800

    Create basic files

---
+++ src/nyaxs/blog/service/TalksService.java
@@ -0,0 +1,12 @@
+package nyaxs.blog.service;
+
+import java.util.List;
+
+import nyaxs.blog.pojo.Talks;
+
+public interface TalksService {
+	int addTalk(Talks talk);
+	int deleteTalk(int id);
+	List<Talks> listTalksByUserId(int userId);
+	List<Talks> listTalksAll();
+}
